dcd562db3b981885fb9e7c6877d91b4123825918
yasmen <yasmen.khaled2000@gmail.com>
2024-07-10 05:12:09 +0200

    first commit

---
+++ public/installer/Views/welcome.blade.php
@@ -0,0 +1,42 @@
+<html>
+
+    <body>
+
+        <div class="container welcome" id="welcome">
+            <div class="initial-display">
+                <p>Welcome to Bagisto</p>
+
+                <div class="content">
+                    <div class="content-container" style="padding: 20px">
+                        <span>
+                        </span>
+
+                        <div class="title">
+                            INTRODUCTION
+                        </div>
+
+                        <span>
+                        </span>
+                    </div>
+                </div>
+
+                <div>
+                    <button type="button" class="prepare-btn" id="welcome-check">Accept</button>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
